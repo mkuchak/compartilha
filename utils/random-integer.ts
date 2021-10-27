073bdcfc008ef9dd4a372713fc12912089d969ba
Marcos Kuchak <mkuchak@hotmail.com>
2021-10-27 18:02:09 -0300

    style: lint

--- utils/random-integer.ts
+++ utils/random-integer.ts
@@ -1,8 +1,8 @@
 // get random number between
 const getRandomInt = (min: number, max: number) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
-export default getRandomInt;
+export default getRandomInt
--- utils/validate-email.ts
+++ utils/validate-email.ts
@@ -1,20 +1,20 @@
 // validate email address
 const validateEmail = async (email: string) => {
   const regex =
-    /^(([^<>()\[\]\\.,;:+\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /^(([^<>()\[\]\\.,;:+\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   try {
     const checkEmail = await fetch(
       `https://verifier.meetchopra.com/verify/${email}?token=${process.env.MEETCHOPRA_API_KEY}`
-    );
-    const checkEmailResult = await checkEmail.json();
+    )
+    const checkEmailResult = await checkEmail.json()
 
     // regex and external api validation
-    return regex.test(String(email).toLowerCase()) && checkEmailResult.status;
+    return regex.test(String(email).toLowerCase()) && checkEmailResult.status
   } catch (e) {
-    console.error(e);
-    return regex.test(String(email).toLowerCase());
+    console.error(e)
+    return regex.test(String(email).toLowerCase())
   }
-};
+}
 
-export default validateEmail;
+export default validateEmail
