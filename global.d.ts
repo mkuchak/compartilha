16270c0fc824d9c6e256c5e5607ea845bc07caef
Marcos Kuchak <mkuchak@hotmail.com>
2021-10-27 14:41:41 -0300

    refactor: add global types for environment variables

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    HASURA_BASE_URL: string;
+    HASURA_GRAPHQL_ADMIN_SECRET: string;
+    HASURA_GRAPHQL_JWT_SECRET: string;
+    HASURA_GRAPHQL_ACCESS_TOKEN_EXPIRES_IN: string;
+    HASURA_GRAPHQL_REFRESH_TOKEN_EXPIRES_IN: string;
+    SENDGRID_API_KEY: string;
+    SENDGRID_SIGNIN_TEMPLATE_ID: string;
+  }
+}
