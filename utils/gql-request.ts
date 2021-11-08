3eda376203a80586fb14fa7578202dd38825190b
Marcos Kuchak <mkuchak@hotmail.com>
2021-11-07 22:44:20 -0300

    chore: updates env vars and separates path from endpoint

--- utils/gql-request.ts
+++ utils/gql-request.ts
@@ -1,14 +1,17 @@
 const gql = async (operation: string, variables: {}) => {
-  const fetchResponse = await fetch(process.env.HASURA_BASE_URL, {
-    method: 'POST',
-    headers: {
-      'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
-    },
-    body: JSON.stringify({
-      query: operation,
-      variables,
-    }),
-  })
+  const fetchResponse = await fetch(
+    `${process.env.HASURA_GRAPHQL_ENDPOINT}/v1/graphql`,
+    {
+      method: 'POST',
+      headers: {
+        'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+      },
+      body: JSON.stringify({
+        query: operation,
+        variables,
+      }),
+    }
+  )
   const data = await fetchResponse.json()
   return data
 }
