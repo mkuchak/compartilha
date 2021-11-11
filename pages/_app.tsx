68fccc514349d4f99f4a21e0fb3821b1ceeccc56
lwtz <w.h@outlook.com.br>
2021-11-11 14:41:35 -0300

    fix: fix formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import 'tailwindcss/tailwind.css'
 import HomePage        from '@/components/HomePage'
 import HomeDescription from '@/components/HomeDescription'
 import TableHome       from '@/components/TableHomePage'
-import CTA from '@/components/CTA'
-import Footer from '@/components/Footer'
+import CTA             from '@/components/CTA'
+import Footer          from '@/components/Footer'
 
 
 function MyApp() {
