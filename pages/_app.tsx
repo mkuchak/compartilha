8c19681791580839924e4046307bf2bee4303c3a
lwtz <w.h@outlook.com.br>
2021-11-11 14:38:47 -0300

    chore: add all components to homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import HomePage        from '@/components/HomePage'
+import HomeDescription from '@/components/HomeDescription'
+import TableHome       from '@/components/TableHomePage'
+import CTA from '@/components/CTA'
+import Footer from '@/components/Footer'
+
+
+function MyApp() {
+  return (
+    <>
+      <HomePage />
+      <HomeDescription />
+      <TableHome />
+      <CTA />
+      <Footer />
+    </>
+  )
 }
+
 export default MyApp
